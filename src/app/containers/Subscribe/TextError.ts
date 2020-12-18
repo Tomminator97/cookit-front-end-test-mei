edd896d98b433a4beb8d3250bc1bea8a1d2849d9
Tommy Mei <tommymei618@hotmail.com>
2020-12-18 09:42:50 -0500

    added new confirmation page, new TextError tag

---
+++ src/app/containers/Subscribe/TextError.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro';
+
+export const TextError = styled.p`
+  font-size: 1rem;
+  line-height: 1.5;
+  color: red;
+  margin: 0.625rem 0 1.5rem 0;
+  font-size: small;
+`;
